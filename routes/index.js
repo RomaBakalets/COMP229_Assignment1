abc0220c7198e3ddf215225e0e3e4e69c7f7ef07
Roma044 <roma.bakalets0406@gmail.com>
2022-06-01 22:25:00 -0400

    Added Icons

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render(
-    "index",
-
-    {
-      title: "Express",
-      mainHeading: "Main Heading",
-    }
-  );
+  res.render("index", { title: "Home" });
 });
 
+/* GET home page. */
+router.get("/home", function (req, res, next) {
+  res.render("index", { title: "Home" });
+});
+
+/* GET About Us page. */
+router.get("/about", function (req, res, next) {
+  res.render("index", { title: "About" });
+});
+
+/* GET Products page. */
+router.get("/products", function (req, res, next) {
+  res.render("index", { title: "Products" });
+});
+
+/* GET Services page. */
+router.get("/services", function (req, res, next) {
+  res.render("index", { title: "Services" });
+});
+
+/* GET Contact Us page. */
+router.get("/contact", function (req, res, next) {
+  res.render("index", { title: "Contact" });
+});
 module.exports = router;
